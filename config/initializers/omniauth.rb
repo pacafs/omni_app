b016034a4fd740bf82584e84b6c96be6c0341760
Pedro Fernandes da Silva <paca.fs@gmail.com>
2015-09-07 10:55:03 +0100

    Initializer.rb

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,5 +1,5 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :twitter, '0uXO9JWS0eNJWiQ67hbWeGEhj', 'KAhNZ5DFItwbNqEK8jsO38l6X1kxNuqYypAegwKPSh2sLyF1Un'
-  provider :facebook, '382748711909197', '3a3e6bd52e23396dafff399585e1e21e'
-  provider :github, '2f1bc12db09703300721', 'd7773f7101a7fdf72cb8effe10ba3086c43f3880'
+  provider :twitter, '...', '...'
+  provider :facebook, '...', '...'
+  provider :github, '...', '...'
 end
